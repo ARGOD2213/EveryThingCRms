c9cf0caef666f755cbaee535f376814ddc79b8ba
Mahindra Chintala <127439352+ARGOD2213@users.noreply.github.com>
2026-07-05 23:40:19 +0530

    Committing basic Project Structure

---
+++ src/main/java/com/everythingcrms/backend/EveryThingCRmsApplication.java
@@ -0,0 +1,13 @@
+package com.everythingcrms.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryThingCRmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EveryThingCRmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/everythingcrms/backend/EveryThingCRmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everythingcrms.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveryThingCRmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
