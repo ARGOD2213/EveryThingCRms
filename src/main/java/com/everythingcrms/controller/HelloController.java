a9f170f3f6b2895654988c092c2ea6a085579066
Mahindra Chintala <127439352+ARGOD2213@users.noreply.github.com>
2026-07-07 21:48:13 +0530

    latest

--- src/main/java/com/everythingcrms/controller/HelloController.java
+++ src/main/java/com/everythingcrms/controller/HelloController.java
@@ -4,7 +4,9 @@ import com.everythingcrms.entity.UserTest;
 import com.everythingcrms.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import java.util.List;
 
 @RestController
@@ -25,4 +27,22 @@ public class HelloController {
     public List<UserTest> getUsers() {
         return userService.getAllUsers();
     }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<UserTest> getUserById(@PathVariable String id){
+        return userService.getUserById(id).map(ResponseEntity::ok)
+                                        .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("users/{id}")
+    public ResponseEntity<Void> deleteUserById(@PathVariable String id){
+        boolean deleted = userService.deleteUserById(id);
+
+        if(deleted){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+
 }
